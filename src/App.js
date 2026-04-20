452e91b331def2738fa3540e46c9e560e664b4cf
davidgarcia57 <david_3141240195@utd.edu.mx>
2026-04-19 19:24:41 -0600

    cliente agregado

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MainPage from "./components/MainPage";
 import DownloadCenter from "./components/DownloadCenter";
 import MetodologiasSW from "./components/MetodologiasSW";
 
-const GOOGLE_CLIENT_ID = "TU_GOOGLE_CLIENT_ID_AQUI";
+const GOOGLE_CLIENT_ID = "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";
 
 function App() {
   const [view, setView] = useState("main");
@@ -13,25 +13,25 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <div className={view === "metodologias" ? "App" : "App dark-layout"}>
-{view === "main" && (
-        <MainPage
-          onNavigate={() => setView("downloads")}
-          onNavigateMetodologias={() => setView("metodologias")}
-          user={user}
-          onLoginSuccess={(userData) => setUser(userData)}
-          onLogout={() => setUser(null)}
-        />
-      )}
+      <div className={view === "metodologias" ? "App" : "App dark-layout"}>
+        {view === "main" && (
+          <MainPage
+            onNavigate={() => setView("downloads")}
+            onNavigateMetodologias={() => setView("metodologias")}
+            user={user}
+            onLoginSuccess={(userData) => setUser(userData)}
+            onLogout={() => setUser(null)}
+          />
+        )}
 
-      {view === "downloads" && (
-        <DownloadCenter onBack={() => setView("main")} />
-      )}
+        {view === "downloads" && (
+          <DownloadCenter onBack={() => setView("main")} />
+        )}
 
-      {view === "metodologias" && (
-        <MetodologiasSW onBack={() => setView("main")} />
-      )}
-    </div>
+        {view === "metodologias" && (
+          <MetodologiasSW onBack={() => setView("main")} />
+        )}
+      </div>
     </GoogleOAuthProvider>
   );
 }
